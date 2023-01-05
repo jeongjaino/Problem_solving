b350e78ed28e64c655f49841a7eda20794c0abfd
jaino <msb3302@naver.com>
2023-01-05 18:49:23 +0900

    1번 문제 수정 : 테스트케이스 추가로 인한 수정

--- src/main/kotlin/day1/Day1-1.kt
+++ src/main/kotlin/day1/Day1-1.kt
@@ -5,20 +5,19 @@ fun main(){
 
     val br = BufferedReader(InputStreamReader(System.`in`))
 
-    val num = br.readLine().toInt()
-
+    val num = br.readLine().toInt() // 총 인원
     val scaredList = mutableListOf<Int>()
     br.readLine().split(" ").forEach{
         scaredList.add(it.toInt())
-    }
+    } // 공포도 받기
     scaredList.sort() // 오름차순 정렬
 
     var result = 0
     while(scaredList.isNotEmpty()){ // 인원이 존재할 때
-        if(num - scaredList.last() >= 0){ // 총 인원보다 최댓값이 더 작을 때
+        if(scaredList.size >= scaredList.first()){ // 현재 인원이 최대공포도보다 클 때
             result += 1 // 그룹 하나 추가
-            repeat(scaredList.last()){
-                scaredList.removeAt(scaredList.lastIndex) // 마지막 원소(최댓값) 삭제
+            repeat(scaredList.first()){
+                scaredList.removeAt(0) // 마지막 원소(최댓값) 삭제
             }
         }else{ // 총 인원보다 최댓값이 더 크다면
             print(result)
